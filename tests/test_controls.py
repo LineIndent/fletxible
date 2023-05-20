fe6dc963c8d28c621b0a2bf4009a0f5b4c1615f0
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2023-05-20 19:50:38 +0300

    Update controls.py testing.

--- tests/test_controls.py
+++ tests/test_controls.py
@@ -1,18 +1,19 @@
 import unittest
 from logic.controls import title, subtitle, text
+import flet as ft
 
 
 class FletTest(unittest.TestCase):
     def test_title(self):
         result = title("Hello World")
-
+        self.assertIsInstance(result, ft.Text)
         self.assertEqual(result.value, "Hello World")
         self.assertEqual(result.size, 21)
         self.assertEqual(result.weight, "bold")
 
     def test_subtitle(self):
         result = subtitle("Welcome", key="subtitle1")
-
+        self.assertIsInstance(result, ft.Text)
         self.assertEqual(result.value, "Welcome")
         self.assertEqual(result.size, 17)
         self.assertEqual(result.weight, "w700")
@@ -20,7 +21,7 @@ class FletTest(unittest.TestCase):
 
     def test_text(self):
         result = text("Lorem ipsum")
-
+        self.assertIsInstance(result, ft.Text)
         self.assertEqual(result.value, "Lorem ipsum")
         self.assertEqual(result.size, 12)
         self.assertEqual(result.weight, "w400")
