bd7bfa4ed12edfdce7decbceb4e0a9c140d45262
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2023-06-02 16:26:08 +0200

    Changes to preconfig file YAML

--- utilities/config.py
+++ utilities/config.py
@@ -5,6 +5,9 @@ Method: Returns a string to create the fx_config.yml file
 
 def create_yaml_file():
     string = """
+  site-name: "fletxible."
+  repo-url: "https://github.com/LineIndent/fletxible"
+    
   navigation:
     - Home: "index.py"
     - About: "about.py"
